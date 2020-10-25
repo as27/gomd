68606beef9c8b8b1cd4114d017e2c807d813f7c6
Andreas Schröpfer <andreas@apxp.info>
2020-10-25 10:32:04 +0100

    #29 verbose mode added

--- cmd/gomd/main.go
+++ cmd/gomd/main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 )
 
+var (
+	flagVerbose = flag.Bool("verbose", false, "start in verbose mode. Will print internal logs in the app. Use this for debugging purposes.")
+)
+
 func main() {
+	flag.Parse()
 	a := newApp()
 	w, ok := a.log.(io.Writer)
 	if ok {
-		log.SetOutput(w)
+		if *flagVerbose {
+			log.SetOutput(w)
+		}
 	}
+	if !*flagVerbose {
+		log.SetOutput(ioutil.Discard)
+	}
+	a.conf.verbose = *flagVerbose
 	err := a.run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "error running the app: ", err)
